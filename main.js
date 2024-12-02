e9221659bd8187654533a709dfa95eae27e9b02c
mdev896 <qbert.qwerty.1@gmail.com>
2024-12-02 20:16:20 +0300

    changed picture order

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
     let div = document.getElementById("insider");
     for (let i = 1; i <= 40; i++) {
+        if (i == 2) continue;
         div.innerHTML +=
             '<div><img src="photos/' + i + '.jpg" class="img"/></div> ';
     }
+    div.innerHTML += '<div><img src="photos/2.jpg" class="img"/></div> ';
     /*
 	div.innerHTML +=
         '<div><img src="photos/1.jpg"style="width: 100%; border-radius: 12px"/></div> ';
